8eafa228382a2c563de0646c738869813400d5db
Dan <dponce6@gmail.com>
2017-07-04 10:49:38 -0400

    Fixed test class

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidEndpointTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AndroidEndpointTest.java
@@ -1,25 +1,38 @@
 package com.udacity.gradle.builditbigger;
 
 import android.support.test.runner.AndroidJUnit4;
+import android.text.TextUtils;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static junit.framework.Assert.assertNotNull;
+import java.util.concurrent.CountDownLatch;
+
+import static junit.framework.Assert.assertFalse;
 
 @RunWith(AndroidJUnit4.class)
 public class AndroidEndpointTest implements TaskCompleted
 {
+    String mResultString = null;
+    CountDownLatch mSignal = null;
+
     @Test
-    public void testAsyncTaskResult() throws Exception
+    public void testAsyncTaskResult() throws InterruptedException
     {
+        mSignal = new CountDownLatch(1);
+
         JokeAsyncTask jokeAsyncTask = new JokeAsyncTask(this);
         jokeAsyncTask.execute();
+
+        mSignal.await();
+
+        assertFalse(TextUtils.isEmpty(mResultString));
     }
 
     @Override
     public void onTaskCompleted(String result)
     {
-        assertNotNull(result.isEmpty());
+        mResultString = result;
+        mSignal.countDown();
     }
 }
